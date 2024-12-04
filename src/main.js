e2d4fc14432fd60f273ecf014604da8f9cd3b93f
songla <473033007@qq.com>
2024-12-05 02:01:48 +0800

    deploy

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ECharts from 'vue-echarts'
+import 'echarts'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('v-chart', ECharts)
+app.mount('#app')
+
